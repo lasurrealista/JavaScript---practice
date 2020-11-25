023b67f01a851bc61948b403bcdb4112bd727c4f
lasurrealista <ladyblue@freemail.hu>
2020-11-25 14:49:57 +0100

    New exercises and solutions

--- strings/strings.js
+++ strings/strings.js
@@ -12,15 +12,18 @@ const janeDoe = {
     lastName: 'Doe'
 };
 
+const merge = ( ...objects ) => ( { ...objects } );
+console.log(merge(johnDoe, janeDoe));
+
+/* 
 function transform(...objects) {
     return {
       ...[...objects]
         .map((item, index) => index = item)
     };
   }
-  console.log(transform(johnDoe, janeDoe));
+console.log(transform(johnDoe, janeDoe));
 
-/* 
 --->
     {
         0: { 
@@ -73,10 +76,40 @@ console.log(removeStringContainingA);
 const removeWhiteSpace = removeStringContainingA.map(item => item.trimEnd());
 console.log(removeWhiteSpace);
 
-*/
+----
 
 const newArray = (sampleArray1.concat(sampleArray2, sampleArray3)).filter((item, index) => (sampleArray1.concat(sampleArray2, sampleArray3)).indexOf(item) == index).filter(item => !item.includes(character)).map(item => item.trimEnd());
 console.log(newArray);
 
+*/
+
+function makeNewArray(char, ...arrays) {
+    const newArray = arrays.flat()
+    return newArray
+    .filter((item, index) => newArray.indexOf(item) == index)
+    .filter(item => !item.includes(char))
+    .map(item => item.trimEnd());
+};
+console.log(makeNewArray(character, sampleArray1, sampleArray2, sampleArray3));
+
 /* 4 */
 
+function mergeArrays(char, ...arrays) {
+    const onlyArraysMerged = arrays.flat();
+    const charArray = Array.from(char);
+    const mergedArray = charArray.concat(onlyArraysMerged);
+    return mergedArray;
+};
+console.log(mergeArrays('a', ['egyszer', 'kétszer'], ['ötször, hatszor']));
+
+function removeDuplicatesFromArray(array) {
+    const duplicatesRemovedFromArray = array.filter((item, index) => array.indexOf(item) == index);
+    return duplicatesRemovedFromArray;
+};
+console.log(removeDuplicatesFromArray(['helló', 'szia', 'viszlát, helló', 'szia', 'hogy vagy']));
+
+function trimStartEnd(array) {
+    const trimmedStartEnd = array.map(item => item.trim());
+    return trimmedStartEnd;
+};
+console.log(trimStartEnd(['    jaj, tervezőm', 'R2-D2    ', '   3-CPO   ']));
