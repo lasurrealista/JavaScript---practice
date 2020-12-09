d1373a2d599e984e2e97bcd474c8bd6d4e74bc2d
lasurrealista <ladyblue@freemail.hu>
2020-12-09 14:08:41 +0100

    module-systems

--- module-systems/app.js
+++ module-systems/app.js
@@ -1,4 +1,14 @@
 'use strict';
 
-import { } from './module.js';
+import { setCookie } from './module.js';
 
+setCookie('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
+console.log(document.cookie);
+
+import cookieHandler from './module.js';
+
+cookieHandler.saveToSessionStorage(document.cookie);
+
+import { sum } from './module.js';
+
+console.log(sum(15, 60));
--- module-systems/module.js
+++ module-systems/module.js
@@ -1,11 +1,50 @@
 'use strict';
 
-export {
-    
-}
+/* 1 */
 
-export default
+export const setCookie = (name, value, expirationDays = 1, path = '/') => {
+    const now = new Date();
+    now.setTime(now.getTime() + ((expirationDays * 24 * 60 * 60 * 1000) - 85500000));
+    const expires = now.toUTCString();
+    document.cookie = `${name}=${value};expires=${expires};path=${path}`;
+};
 
-export {
-    ... as ...
+/* 2 */
+
+document.cookie = 'uid=354774631237; expires=Wed, 09 Dec 2020 18:00:00 UTC; path=/';
+document.cookie = 'ssid=Bx55OWbHJ0Vt_IGIFÍ; expires=Wed, 09 Dec 2020 18:00:00 UTC; path=/';
+document.cookie = 'viewed=5; expires=Wed, 09 Dec 2020 18:00:00 UTC; path=/';
+
+const cookieHandler = {
+    getCookie() {
+        const cutString = document.cookie.replace(/; /g, '", "').replace(/=/g, '":"');
+        const cutString2 = `{ "${cutString}" }`;
+        return JSON.parse(cutString2);
+    },
+    saveToSessionStorage() {
+        const cutString = document.cookie.replace(/; /g, '", "').replace(/=/g, '":"');
+        const cutString2 = `{ "${cutString}" }`;
+        const object = JSON.parse(cutString2);
+        const entries = Object.entries(object);
+        return entries.map((key, value) => sessionStorage.setItem(key, value));
+    },
+    deleteCookie(name) {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    },
+    clearCookies() {
+        const cookies = document.cookie.split("; ");
+        for (let i = 0; i < cookies.length; i++){ 
+            cookieHandler.deleteCookie(document.cookie);
+        };
+    },
+};
+
+export default cookieHandler;
+
+/* 3 */
+
+const addNumbers = (a, b) => a + b;
+
+export { 
+    addNumbers as sum
 };
